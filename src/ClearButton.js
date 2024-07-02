29a3b40bf257ea84c7c0867f26aaed641dc25427
mortega16 <146567049+mortega16@users.noreply.github.com>
2024-07-02 15:21:09 -0600

    cambios para que tabla sea dinamica
    cambios para que tabla sea dinamica

---
+++ src/ClearButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ClearButton = ({ onClick }) => {
+    return (
+        <button onClick={onClick}>Borrar Todo</button>
+    );
+};
+
+export default ClearButton;
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import PixelArt from './PixelArt';
+
+ReactDOM.render(<PixelArt />, document.getElementById('root'));
